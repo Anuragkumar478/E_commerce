7d8a5ec4c19b27ad333d8ac761d8305b8e521e09
Anurag kumar <kumaranurag9795@gmail.com>
2026-08-17 16:59:20 +0530

    changin in cors policy use

--- Backend/server.js
+++ Backend/server.js
@@ -34,8 +34,9 @@ app.set('io', io);
 // Middleware
 // app.use(cors());
 app.use(cors({
-  origin:[ "http://localhost:5173",
-   "https://e-commerce-h9ke.vercel.app"
+  origin: [
+    "http://localhost:5173",
+    "https://e-commerce-h9ke-git-main-anurag-kumars-projects-4fdb0a50.vercel.app"
   ],
   credentials: true
 }));
